db7d484f41b74f53cc29b835bcf5b5f338d8365c
Jean Carlos Trejo <jeancarlostrejo19@gmail.com>
2026-01-01 21:58:07 -0400

    add TaskResource for transforming task data into JSON format

---
+++ app/Http/Resources/TaskResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TaskResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'description' => $this->description,
+            'is_completed' => (bool) $this->is_completed,
+            'completed_at' => $this->completed_at?->format('d/m/Y H:i:s'),
+            'created_at' => $this->created_at->format('d/m/Y H:i:s'),
+            'updated_at' => $this->updated_at->format('d/m/Y H:i:s'),
+        ];
+    }
+}
