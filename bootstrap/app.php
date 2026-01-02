f1bb72f700ebab2fb88af7544c8168fd401eea18
Jean Carlos Trejo <jeancarlostrejo19@gmail.com>
2026-01-01 22:11:44 -0400

    add error handling for ModelNotFound exception for route model binding

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,19 +1,31 @@
 <?php
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
-        //
+    ->withExceptions(function (Exceptions $exceptions) {
+        $exceptions->render(function (NotFoundHttpException $e, Request $request) {
+            if (app()->isProduction()) {
+                if ($request->is('api/*') && ($e->getPrevious() instanceof ModelNotFoundException) && $request->expectsJson()) {
+                    return response()->json([
+                        'message' => 'Resource Not Found'
+                    ], Response::HTTP_NOT_FOUND);
+                }
+            }
+        });
     })->create();
